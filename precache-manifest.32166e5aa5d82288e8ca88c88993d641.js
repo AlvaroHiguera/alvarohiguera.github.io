450f28e41c8f954681e93722ce4a0c740f345581
Alvaro <ahigueraherrero@infomm.local>
2020-01-28 16:57:04 +0100

    Version 1.0 de la sesion 37

---
+++ precache-manifest.32166e5aa5d82288e8ca88c88993d641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684ccfbf96ef5ca29ee045971d6449ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "066a3aa7bd81271020f5",
+    "url": "/static/css/main.08011531.chunk.css"
+  },
+  {
+    "revision": "4694cfe75fcdc553d01b",
+    "url": "/static/js/2.0e83cf07.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0e83cf07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "066a3aa7bd81271020f5",
+    "url": "/static/js/main.7068ec26.chunk.js"
+  },
+  {
+    "revision": "116a960543ebc9ac1e97",
+    "url": "/static/js/runtime-main.32896179.js"
+  }
+]);
